79ed021d709a9051402cc649a93dcbaa0c649ac1
Juan Carlos <rizomendozajuancarlos255@gmail.com>
2025-07-02 12:01:07 -0600

    Delete API directory

--- API/AGDV-Document-Management/src/main/java/com/agdv/AGDV/Document/Management/repository/DocumentCarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agdv.AGDV.Document.Management.repository;
-
-import com.agdv.AGDV.Document.Management.model.DocumentCar;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface DocumentCarRepository extends MongoRepository<DocumentCar, String> {
-    List<DocumentCar> findByCarIdAndAsset(String carId, int asset);
-}
--- API/AGDV-Document-Management/src/main/java/com/agdv/AGDV/Document/Management/repository/InsuranceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.agdv.AGDV.Document.Management.repository;
-
-import com.agdv.AGDV.Document.Management.model.Insurance;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InsuranceRepository extends MongoRepository<Insurance, Integer> {
-    // Puedes agregar métodos personalizados si necesitas búsquedas más específicas
-}
